c8fd340814689244af30a854a4bfb43d619cd0bc
SHARMA PRATEEK KAVINDRA <prateeks@osl-40.(none)>
2010-11-23 16:43:47 +0530

    work to be done on markers for users

--- src/dcad/ui/main/EditView.java
+++ src/dcad/ui/main/EditView.java
@@ -37,9 +37,13 @@ import dcad.model.constraint.RelativeConstraint;
 import dcad.model.geometry.AnchorPoint;
 import dcad.model.geometry.GeometryElement;
 import dcad.model.geometry.Stroke;
+import dcad.model.geometry.segment.SegCircleCurve;
 import dcad.model.geometry.segment.SegLine;
 import dcad.model.geometry.segment.Segment;
+import dcad.process.ProcessManager;
 import dcad.process.io.Command;
+import dcad.process.recognition.RecognitionManager;
+import dcad.process.recognition.marker.MarkerRecognizer;
 import dcad.process.recognition.segment.ConvertSegment;
 import dcad.process.recognition.stroke.ConvertStrokeType;
 import dcad.ui.drawing.*;
@@ -119,12 +123,12 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 				label2 = "Angle" ; 
 
 				// UI now
-				jlabel1 = new JLabel("Length :");
+				jlabel1 = new JLabel("Length");
 				jfield1 = new JTextField(5);
 				jChange1 = new JButton("Change"); 	jChange1.setActionCommand("lChange") ; 
 				jReset1 = new JButton("Reset");		jReset1.setActionCommand("lReset" ) ; 
 
-				jlabel2 = new JLabel("Angle :");
+				jlabel2 = new JLabel("Angle");
 				jfield2 = new JTextField(5);
 				jChange2 = new JButton("Change"); 	jChange2.setActionCommand("aChange") ; 
 				jReset2 = new JButton("Reset");   	jReset2.setActionCommand("aReset") ; 
@@ -135,15 +139,15 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 				label1 = "Centre" ;
 				label2 = "Radius" ;
 
-				jlabel1 = new JLabel("Centre :");
+				jlabel1 = new JLabel("Radius");
 				jfield1 = new JTextField(10);
-				jChange1 = new JButton("Change"); 	jChange1.setActionCommand("cChange") ;
-				jReset1 = new JButton("Reset");		jReset1.setActionCommand("cReset") ;
+				jChange1 = new JButton("Change"); 	jChange1.setActionCommand("rChange") ;
+				jReset1 = new JButton("Reset");		jReset1.setActionCommand("rReset") ;
 
-				jlabel2 = new JLabel("Radius");
+				jlabel2 = new JLabel("Arc Angle");
 				jfield2 = new JTextField(5);
-				jChange2 = new JButton("Change");	jChange2.setActionCommand("rChange") ;
-				jReset2 = new JButton("Reset");     jReset2.setActionCommand("rReset") ;
+				jChange2 = new JButton("Change");	jChange2.setActionCommand("aaChange") ;
+				jReset2 = new JButton("Reset");     jReset2.setActionCommand("aaReset") ;
 			}	
 			jChange1.addActionListener(ev) ; jReset1.addActionListener(ev) ; jChange2.addActionListener(ev) ;  jReset2.addActionListener(ev) ;
 
@@ -294,18 +298,15 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 		else if(seg_type==Segment.CIRCLE) {
 			//CIRCLE.
 			getCircleProperties() ;
+			
 		} 
-		UIUpdate();
+		updateUI();
+		dv.repaint();
 	}	
 
 
 	/******************************************************************************/
 
-
-	public void getCircleProperties ()
-	{
-
-	}
 
 
 	/*********************************************************************************/
@@ -394,77 +395,87 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 
 	}
 
+ 	/*********************************************************************************/
+
+ 	public void getCircleProperties ()
+ 	{
+		String radius = "" ;
+		String angle = "" ;
+		String centre = "" ;
+		int level1 = PROPERTY ;
+		int level2 = PROPERTY ;
+		
+		Vector constraints = new Vector();
+		constraints = seg.getM_constraints();
+		Iterator itr = constraints.iterator();
+		while (itr.hasNext())
+		{
+			String cons = itr.next().toString();
+			String parsedCons[];
+			
+			parsedCons = cons.split("[ ]+");
+			for(int i =0; i < parsedCons.length ; i++)
+			{
+				///System.out.println(parsedCons[i]);
+				if((parsedCons[1].compareToIgnoreCase("HARD") == 0) && (parsedCons[3].compareToIgnoreCase("Arc") == 0) && (parsedCons[4].compareToIgnoreCase("Radius") == 0)){
+					radius = parsedCons[5];
+					level1 = HARD ;
+				}
+				if((parsedCons[1].compareToIgnoreCase("HARD") == 0) && (parsedCons[3].compareToIgnoreCase("Arc") == 0) && (parsedCons[4].compareToIgnoreCase("Angle") == 0)){
+					angle = parsedCons[5];
+					level2 = HARD ;
+				}
+			} //NOT HARD
+			
+		}
+		
+
+		SegCircleCurve circle = (SegCircleCurve) this.seg ;
+		centre = circle.getM_center().getM_point().toString();
+		DecimalFormat twoDForm = new DecimalFormat("#.##");
+
+		if(level1==HARD && level2==HARD) {
+
+			seg_properties.set(1,radius,level1); 
+			seg_properties.set(2,angle,level2) ;
+			return ;
+		}
+
+		if(level1!=HARD) {
+			radius = Double.valueOf(twoDForm.format(circle.getM_radius()/GConstants.cmScaleDrawingRatio)).toString(); 
+			level1=PROPERTY ;
+		}
+ 
+		if(level2!=HARD) {
+			angle = Double.valueOf(twoDForm.format(circle.getM_arcAngle())).toString(); 
+			level2=PROPERTY;
+		}
+
+		seg_properties.set(1,radius,level1); 
+		seg_properties.set(2,angle,level2) ;
+		
+ 	}
+ 
+
 	/************************************************************************************/
-	/**
-	 * apply field values to the segment/stroke. Done using the command-queue .
-	 * 
-	 */
-	public void ChangeProperty(int seg_type , String property)
-	{
-		String cmd = "" ;
-		Point pt = this.pt ;
-		/*****LINE****/
-		if(seg_type==Segment.LINE) 
-		{
-
-			if(property=="length")
-			{
-				String length ;
-				length = seg_properties.jfield1.getText().trim() ;
-				if (length.compareTo(seg_properties.field1)==0) //field hasnt changed 
-				{
-					System.out.println("NOT CHANGED") ;
-				}
-				else 
-				{
-					cmd = length ;
-					// magic happens!
-					System.out.println("POINT VALUE IS"+pt.toString()) ;
-					dv.writeText((int)pt.getX(),(int) pt.getY(), cmd) ;
-					dv.repaint();
-					//Update Properties ?
-				}
-			}
-
-			else if (property == "angle") 
-			{
-				String angle ;
-				angle = seg_properties.jfield2.getText().trim();
-				if (angle.compareTo(seg_properties.field2)==0) //field hasnt changed 
-				{
-					System.out.println("NOT CHANGED") ;
-				}
-				else 
-				{
-					cmd = angle+"a" ;
-					dv.writeText((int)pt.getX(),(int) pt.getY(), cmd);
-				}
-			}
-			//CLEAR everything?
-		}
-
-		/*****CIRCLE*******/
-		else if (seg_type==Segment.CIRCLE) 
-		{
-			if(property=="centre") {
-			}
-			else if (property=="radius") {
-			}
-		}
-	}
 
 	public void ChangeProperty2(int seg_type,String property)
 	{
 		String val="" ;
-		if (property=="length") {
-			val = seg_properties.jfield1.getText().trim();
+		if (property=="length" || property =="radius") {
+ 			val = seg_properties.jfield1.getText().trim();
+ 		}
+		else if (property == "angle" || property == "angle") {
+			val = seg_properties.jfield2.getText().trim() ;
 		}
+	
+
 		dv.A.A_change_Seg_property(seg, property, val) ;
-		Vector c= new Vector() ;
 
 		UIUpdate() ;
 	}
 
+ 	/*********************************************************************************/
 	
 	/**
 	 * Change to type. Processes stroke all over again, but side-steps the recognition process. 
@@ -481,10 +492,19 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 		/** Try processing the stroke all over again. deletes the segment currently drawn and runs the segmentation and constraint
 		 * algorithms all over again. Bypasses segmentation algorithm in Stroke.recognizesegment because user_given flag is set.
 		 */
-		dv.ProcessStroke(strk) ;
+		dv.Process_Stroke(strk) ;
+	//	displayOptions(strk.getM_segList().elementAt(0), pt) ;
+		UIUpdate() ;
 
 	}
 
+ 	/*********************************************************************************/
+ 
+	/**
+	 * Change stroke to marker. Whether it really is a marker is difficult to guarantee.
+	 * @param seg
+	 * @return
+	 */
 	public int ChangeToMarker(Segment seg) 
 	{
 		Stroke strk = seg.getM_parentStk() ;
@@ -492,8 +512,22 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 		ConvertStrokeType convStrokeType = new ConvertStrokeType();
 		convStrokeType.ConvertLastDrawnStroke();
 		convStrokeType.ConvertStroke(seg.getM_parentStk()) ;
+//		ConvertStrokeType convStrokeType = new ConvertStrokeType();
+//		convStrokeType.ConvertLastDrawnStroke();
+//		convStrokeType.ConvertStroke(seg.getM_parentStk()) ;
+ 
+		RecognitionManager recogMan = ProcessManager.getInstance().getRecogManager();
+		MarkerRecognizer mrkrecog =  recogMan.getMarkerRecognitionMan().getMarkerRecognizer();
+		Vector markers = mrkrecog.user_specified_marker(strk) ;
+		
+		display_marker_options(markers) ;
 
 		return 1;
+	}
+
+	public int set_as_marker(int index, Vector markers)
+	{
+		
 	}
 	
 	/****************************************************************************/
@@ -525,130 +559,6 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 		}
 		return -1;
 	}
-
-	/**Function to perform various checks (add/remove constraints)
-	 * after user clicks the submit button
-	 */
-	public void ChangeLineProperties (JTextField textLineLength, JTextField textLineAngle)
-	{
-
-		//	dv.logEvent("LineParameterWindow|performSubmitActionLineParam({java.awt.TextField}" 
-		//			+ textLineLength + ", {java.awt.TextField}" + textLineAngle + ");");
-
-		Constraint cons = null;
-		int listIndex = -1;
-
-		double textAngle = ANGLE_TEXT_BOX_NULL;
-		if((textLineAngle.getText().trim().isEmpty()) ){
-			textAngle = ANGLE_TEXT_BOX_NULL;  // -1 saying that textBox is NULL 
-		}
-		else{
-			textAngle = Double.parseDouble(textLineAngle.getText().trim()); 
-		}
-
-		// Checks whether textbox is empty
-		double textLength = LENGTH_TEXT_BOX_NULL;
-		if((textLineLength.getText().trim().isEmpty()) || Double.compare(Double.parseDouble(textLineLength.getText().trim()),0.0) ==0){
-			textLength = LENGTH_TEXT_BOX_NULL;  // -1 saying that textBox is NULL 
-		}
-		else{
-			textLength = Double.parseDouble(textLineLength.getText().trim()); 
-		}
-
-		// 29-04-10
-		// If there is any length constraint on a line and we try to add angle constraint it could not
-		// add it. This condition is to check whether a length constraint exists, delete it and again
-		// it in order - angle then length 
-		if((Double.compare(length,textLength) == 0) && (Double.compare(length, LENGTH_TEXT_BOX_NULL) !=0) && (Double.compare(angle,ANGLE_TEXT_BOX_NULL) == 0) && (!(textLineAngle.getText().trim().isEmpty()))){
-			length = LENGTH_TEXT_BOX_NULL;
-			cons = getSegmentConstraint("line", "length",seg);
-			// find the index of constraint in recognize view constraint list
-			if(cons != null){
-				listIndex = getListConstraintIndex(cons);
-				if(listIndex !=-1){
-					MainWindow.getRecognizedView().deleteConstraint(listIndex);
-				}
-			}
-		}
-
-		// if angle is same or (angle text box is blank and previously also it was blank )
-		if((Double.compare(angle,textAngle) == 0)  
-				|| ((Double.compare(textAngle, ANGLE_TEXT_BOX_NULL) == 0 ) 
-						&& (Double.compare(angle,ANGLE_TEXT_BOX_NULL) ==0))){
-			System.out.println("Angle is same");
-		}
-		// if angle text box is now blank and previously it was not blank then remove angle constraint 
-		else if((Double.compare(textAngle, ANGLE_TEXT_BOX_NULL)==0) && (Double.compare(angle,ANGLE_TEXT_BOX_NULL) !=0)){
-			System.out.println("remove angle constraint");
-			cons = getSegmentConstraint("line", "angle",seg);
-			if(cons == null){
-				cons = getSegmentConstraint("vertical", "line",seg);
-				if(cons == null){
-					cons = getSegmentConstraint("horizontal", "line",seg);
-				}
-			}
-
-			// find the index of constraint in recognize view constraint list
-			listIndex = getListConstraintIndex(cons);
-			if(listIndex !=-1){
-				MainWindow.getRecognizedView().deleteConstraint(listIndex);
-				dv.repaint();
-			}
-
-		}
-		else
-		{
-			angleString = textLineAngle.getText().trim() + "a";
-			//dv.writeText((int)seg.getSegStart().getX(), (int)seg.getSegStart().getY(), angleString);
-			dv.writeText((int)pt.getX(),(int) pt.getY(), angleString);
-			dv.repaint();
-			System.out.println("Angle is changed to " +  angleString);
-		}
-
-		// 24-05-10
-		//this was causing problems
-		// once the angle is shifted 
-		// line's length constraint was not set because point was not near to the segment 
-		pt.setLocation((seg.getSegStart().getX()+seg.getSegEnd().getX())/2, (seg.getSegStart().getY()+seg.getSegEnd().getY())/2);
-
-		// checks if the value text box contains and the previous value are same
-		// 1. if the textbox's value is same as previous value
-		// or 2. if Textbox is null and previous it was also null
-		// do nothing
-		if((Double.compare(length,textLength) == 0)  
-				|| ((Double.compare(textLength, LENGTH_TEXT_BOX_NULL) == 0 ) 
-						&& (Double.compare(length,LENGTH_TEXT_BOX_NULL) ==0))){
-			System.out.println("Line is same");
-		}
-		// if textbox is currently null and previously it had some length
-		// then we need to remove that length constraint
-		else if((Double.compare(textLength, LENGTH_TEXT_BOX_NULL)==0) && (Double.compare(length,0.0) !=0)){
-			System.out.println("remove constraint");
-			// get the actual constraint
-			cons = getSegmentConstraint("line", "length",seg);
-			// find the index of constraint in recognize view constraint list
-			listIndex = getListConstraintIndex(cons);
-			if(listIndex !=-1){
-				MainWindow.getRecognizedView().deleteConstraint(listIndex);
-				dv.repaint();
-				System.out.println("constraint removed ");
-			}
-		}
-		// simply add the constraint and remove if any previous length constraint added
-		else{
-			lengthString = textLineLength.getText().trim();
-			//dv.writeText((int)seg.getSegStart().getX(), (int)seg.getSegStart().getY(), lengthString);
-			dv.writeText((int)pt.getX(),(int) pt.getY(), lengthString);
-			dv.repaint();
-			System.out.println("Length is changed to " +  lengthString);
-		}
-
-		//dv.logEvent(Command.PAUSE);
-		System.out.println("Submit Clicked");
-
-	}
-
-
 
 	/**Function to get particular constraint on an element if it exists
 	 * @author Sunil Kumar
@@ -691,11 +601,10 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 		
 		else if (cmd.compareToIgnoreCase("lChange")==0) {
 			ChangeProperty2(Segment.LINE,"length") ;
-			UIUpdate();
-			displayOptions(seg, pt);
+
 		}
 		else if(cmd.compareToIgnoreCase("aChange")==0) {
-			ChangeProperty(Segment.LINE,"angle") ;
+			ChangeProperty2(Segment.LINE,"angle") ;
 		}
 		else if (cmd.compareToIgnoreCase("lReset")==0) {
 			String original = seg_properties.field1 ;
@@ -708,17 +617,17 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 		 
 		/******************* CIRCLE *********************/
 		 
-		else if(cmd.compareToIgnoreCase("cChange")==0) {
-			ChangeProperty(Segment.CIRCLE,"centre") ;
+		else if(cmd.compareToIgnoreCase("aaChange")==0) {
+			ChangeProperty2(Segment.CIRCLE,"angle") ;
 		}
 		else if(cmd.compareToIgnoreCase("rChange")==0) {
-			ChangeProperty(Segment.CIRCLE,"radius") ;
+			ChangeProperty2(Segment.CIRCLE,"radius") ;
 		}
-		else if (cmd.compareToIgnoreCase("cReset")==0) {
+		else if (cmd.compareToIgnoreCase("rReset")==0) {
 			String original = seg_properties.field1 ;
 			seg_properties.reset(1) ;
 		}
-		else if (cmd.compareToIgnoreCase("rReset")==0) {
+		else if (cmd.compareToIgnoreCase("aaReset")==0) {
 			String original = seg_properties.field2 ;   
 			seg_properties.reset(2) ;
 		}
@@ -729,6 +638,8 @@ public class EditView extends JPanel implements ActionListener,MouseListener,Mou
 			//	performCancelActionLineParam();
 			//	dv.setParameterWinBitSet(false);
 		}
+	UIUpdate();
+	displayOptions(seg, pt);
 		//dv.logEvent(Command.PAUSE);
 	}
 	
